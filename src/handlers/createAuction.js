3497718414e05f4adf6e2cbdf939347d390279ef
pavanleton <pavankumarkpkp@gmail.com>
2021-08-22 13:49:08 +0530

    create auction API

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,16 @@
+async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    title,
+    status: "OPEN",
+    createdAt: now.toISOString(),
+  };
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = createAuction;
