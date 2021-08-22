dc3e04288e723c2e69a64cc4f831a755f6adeed9
pavanleton <pavankumarkpkp@gmail.com>
2021-08-22 14:23:18 +0530

    setup lambda IAM role to access dynamo db

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,12 +1,21 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: "OPEN",
     createdAt: now.toISOString(),
   };
+
+  await dynamoDB.put({ TableName: "AuctionsTable", Item: auction }).promise();
+
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
